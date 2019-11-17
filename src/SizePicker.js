098215a581ad967bac230e6c64f006c9319433c4
Mateusz Hadryś <kugot999@gmail.com>
2019-11-17 13:27:08 +0100

    create a basic functional size picker (doesn't update)

--- src/SizePicker.js
+++ src/SizePicker.js
@@ -1,7 +1,21 @@
 import React from "react"
+import InputRange from "react-input-range"
+import "react-input-range/lib/css/index.css"
+import throttle from "lodash.throttle"
 
 const SizePicker = ({ targetSize, setTargetSize }) => {
-	return <div />
+	const throttledUpdate = throttle(setTargetSize, 200, { leading: true })
+
+	const onChange = (value) => {
+		throttledUpdate(value)
+	}
+
+	return (
+		<div style={{ margin: "30px 0" }}>
+			{/* TODO: base the min and max values on the canvas size */}
+			<InputRange maxValue={400} minValue={20} value={targetSize} onChange={onChange} />
+		</div>
+	)
 }
 
 export default SizePicker
