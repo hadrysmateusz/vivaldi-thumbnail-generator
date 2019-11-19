0d2568413659b20eaa2e5fceeb9b2806e3ad78fa
Mateusz Hadryś <kugot999@gmail.com>
2019-11-19 23:12:23 +0100

    create some style utilities

---
+++ src/styleUtils.js
@@ -0,0 +1,15 @@
+import { css } from "styled-components"
+
+export const overlay = css`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+`
+
+export const center = css`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
