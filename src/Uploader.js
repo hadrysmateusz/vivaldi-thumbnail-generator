a5c8d9bc4e74a9ead837a8a003308dc17dc87749
Mateusz Hadryś <kugot999@gmail.com>
2019-11-19 23:12:04 +0100

    style dropzone with styled components

--- src/Uploader.js
+++ src/Uploader.js
@@ -1,7 +1,27 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useDropzone } from "react-dropzone"
+import styled from "styled-components"
 import Thumbnails from "./Thumbnails"
+import { overlay, center } from "./styleUtils"
+
+const DropzoneContainer = styled.div`
+	position: relative;
+	height: 200px;
+	border: 1px dashed #333;
+`
+
+const Overlay = styled.div`
+	${overlay}
+	${center}
+	z-index: 80;
+	background: rgba(0, 0, 0, 0.36);
+	color: white;
+	font-size: 1.6em;
+	font-weight: bold;
+	letter-spacing: 0.05em;
+	text-transform: uppercase;
+`
 
 const Uploader = ({ setImageUrls, imageUrls }) => {
 	const onDrop = (acceptedFiles) => {
@@ -29,7 +49,7 @@ const Uploader = ({ setImageUrls, imageUrls }) => {
 		setImageUrls([])
 	}
 
-	const { getRootProps, getInputProps, open } = useDropzone({
+	const { getRootProps, getInputProps, open, isDragActive } = useDropzone({
 		onDrop,
 		accept: "",
 		noClick: true
@@ -39,10 +59,12 @@ const Uploader = ({ setImageUrls, imageUrls }) => {
 		<div>
 			<button onClick={open}>Add</button>
 			<button onClick={onClear}>Clear</button>
-			<div {...getRootProps({ style: { height: "200px", border: "1px dashed" } })}>
+			<DropzoneContainer {...getRootProps()}>
 				<input {...getInputProps()} />
+				{isDragActive && <Overlay>Drop here to add</Overlay>}
+
 				<Thumbnails imageUrls={imageUrls} />
-			</div>
+			</DropzoneContainer>
 		</div>
 	)
 }
