b5a86264bdbcd4b51376798408baecedf932f9ca
Mateusz Hadryś <kugot999@gmail.com>
2019-11-20 14:58:40 +0100

    add basic header with some copy

--- src/ThumbnailGenerator.js
+++ src/ThumbnailGenerator.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react"
+import styled from "styled-components"
 
 import Downloader from "./Downloader"
 import ColorPicker from "./ColorPicker"
@@ -11,13 +12,57 @@ function ThumbnailGenerator() {
 	const canvasRef = useRef()
 
 	return (
-		<div>
-			<ImageProcessor canvasRef={canvasRef} bgColor={bgColor} targetSize={targetSize} />
-			<SizePicker targetSize={targetSize} setTargetSize={setTargetSize} />
-			<ColorPicker bgColor={bgColor} setBgColor={setBgColor} />
-			<Downloader canvasRef={canvasRef} />
-		</div>
+		<OuterContainer>
+			<InnerContainer>
+				<Headline>Vivaldi Thumbnail Generator</Headline>
+				<Description>
+					Quickly and painlessly generate thumbnails for use in Vivaldi Browser’s Speed
+					Dials
+				</Description>
+				<ImageProcessor canvasRef={canvasRef} bgColor={bgColor} targetSize={targetSize} />
+				<SizePicker targetSize={targetSize} setTargetSize={setTargetSize} />
+				<ColorPicker bgColor={bgColor} setBgColor={setBgColor} />
+				<Downloader canvasRef={canvasRef} />
+			</InnerContainer>
+		</OuterContainer>
 	)
 }
+
+const OuterContainer = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+`
+
+const InnerContainer = styled.div`
+	max-width: 793px;
+	margin: 0 auto;
+`
+
+const Headline = styled.h1`
+	margin: 40px auto 24px;
+
+	font-weight: 900;
+	font-size: 43px;
+	line-height: 48px;
+
+	text-align: center;
+	justify-content: center;
+	letter-spacing: 0.05em;
+	color: #ffffff;
+	text-shadow: 0px 2px 4px rgba(0, 0, 0, 0.06);
+`
+
+const Description = styled.p`
+	max-width: 440px;
+	margin: 24px auto 32px;
+	font-weight: 600;
+	font-size: 18px;
+	line-height: 24px;
+	text-align: center;
+	color: white;
+	text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+`
 
 export default ThumbnailGenerator
