b82028cf86478d6fce6bb0a72cb40dc458b05afe
Mateusz Hadryś <kugot999@gmail.com>
2019-11-27 21:25:16 +0100

    slightly modify header copy

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,13 @@ const App = () => (
 		<Background />
 		<OuterContainer>
 			<InnerContainer>
-				<Headline>Vivaldi Thumbnail Generator</Headline>
-				<Description>
-					Quickly and effortlessly generate thumbnails for use in{" "}
-					<a href="https://vivaldi.com/">Vivaldi Browser’s</a> Speed Dials.
-				</Description>
+				<MainCopy>
+					<Headline>Vivaldi Thumbnail Generator</Headline>
+					<Description>
+						Quickly and effortlessly generate thumbnails for use in{" "}
+						<a href="https://vivaldi.com/">Vivaldi Browser’s</a> Speed Dials.
+					</Description>
+				</MainCopy>
 				<ThumbnailGenerator />
 				<Explainer />
 				<Roadmap />
@@ -24,11 +26,15 @@ const App = () => (
 
 const Container = styled.div``
 
+const MainCopy = styled.div`
+	margin: 52px 0;
+`
+
 const Background = styled.div`
 	height: 600px;
 	position: relative;
 	background-color: #4fd6d6;
-	background-image: url("data:image/svg+xml,%3Csvg width='80' height='80' viewBox='0 0 80 80' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='white' fill-opacity='0.2'%3E%3Cpath d='M50 50c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10s-10-4.477-10-10 4.477-10 10-10zM10 10c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10S0 25.523 0 20s4.477-10 10-10zm10 8c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm40 40c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+	background-image: url("data:image/svg+xml,%3Csvg width='80' height='80' viewBox='0 0 80 80' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='white' fill-opacity='0.15'%3E%3Cpath d='M50 50c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10s-10-4.477-10-10 4.477-10 10-10zM10 10c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10S0 25.523 0 20s4.477-10 10-10zm10 8c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm40 40c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 `
 
 const OuterContainer = styled.div`
