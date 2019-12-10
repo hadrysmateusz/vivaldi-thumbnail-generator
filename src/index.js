14d093e5cf4eaa30c4b3280bd9f2b98d85f950fd
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2019-12-10 15:04:55 +0100

    add service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App"
+import * as serviceWorker from "./serviceWorker"
 
 const rootElement = document.getElementById("root")
 
@@ -9,3 +10,8 @@ if (rootElement.hasChildNodes()) {
 } else {
 	ReactDOM.render(<App />, rootElement)
 }
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register()
