3d0b7ae2e3d2c493a6769ed0e03cf606c6893e1c
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2019-12-10 11:13:08 +0100

    add react-snap

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const rootElement = document.getElementById("root")
+
+if (rootElement.hasChildNodes()) {
+	ReactDOM.hydrate(<App />, rootElement)
+} else {
+	ReactDOM.render(<App />, rootElement)
+}
