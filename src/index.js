406c89e7512f2d220f53cb1a8d2b7c4cf33b256f
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2019-12-13 00:46:44 +0100

    temporarily unregister service worker

--- src/index.js
+++ src/index.js
@@ -14,4 +14,4 @@ if (rootElement.hasChildNodes()) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.unregister()
