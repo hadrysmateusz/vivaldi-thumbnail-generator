3f662468407674c5648c1f4e3efa11798a9195b3
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2019-12-26 22:25:42 +0100

    fix icon canvas rendering

--- src/components/IconCanvas.js
+++ src/components/IconCanvas.js
@@ -7,21 +7,20 @@ import {
 } from "./CanvasCommon"
 import { useSettings, useThumbnails } from "./Generator"
 
-const IconCanvas = ({ icon }) => {
+const IconCanvas = () => {
 	const { scale } = useSettings()
+	const { selected } = useThumbnails()
 	const canvasRef = useRef()
 	useSizeCanvasToCssDimensions(canvasRef)
-
-	const { selected } = useThumbnails()
 
 	useEffect(() => {
 		// clear canvas to remove previous image
 		clearCanvas(canvasRef.current)
 		// if there are no images yet, or they were all removed, exit to prevent errors
-		if (!icon) return
+		if (!selected.image) return
 		// draw the current image with the correct dimensions
 		drawIcon(canvasRef.current, selected.image, scale)
-	}, [icon, scale, selected.image])
+	}, [scale, selected.image])
 
 	return <StyledCanvas ref={canvasRef} />
 }
