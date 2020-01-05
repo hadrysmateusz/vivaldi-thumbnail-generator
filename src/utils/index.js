2fbc38a6f5d2190894b0e367f6207cd744cfaad7
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-01-05 19:54:13 +0100

    fix getHostname import path

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 import loadImage from "./loadImage"
 import makeAsync from "./makeAsync"
 import getBase64FromDataUri from "./getBase64FromDataUri"
-import getHostname from "./getBase64FromDataUri"
+import getHostname from "./getHostname"
 import readFile from "./readFile"
 
 export { loadImage, makeAsync, getBase64FromDataUri, getHostname, readFile }
