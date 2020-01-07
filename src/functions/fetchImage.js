199fa8c912ae46db6b414175c89a84465352ea14
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-01-07 23:16:53 +0100

    attemt the serverless fetchImage function

--- src/functions/fetchImage.js
+++ src/functions/fetchImage.js
@@ -1,6 +1,20 @@
 exports.handler = function(event, context, callback) {
-	callback(null, {
-		statusCode: 200,
-		body: { event, context }
-	})
+	const url = event.queryStringParameters.url
+
+	try {
+		fetch(url, { mode: "cors" })
+			.then((response) => response.blob())
+			.then((blob) => {
+				callback(null, {
+					statusCode: 200,
+					body: { image: blob },
+					headers: { "Access-Control-Allow-Origin": "*" }
+				})
+			})
+	} catch (error) {
+		callback(null, {
+			statusCode: 500,
+			body: { error }
+		})
+	}
 }
