92d12fc0fbf74148653ddefbe16a0be88e86fd32
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-01-12 14:57:05 +0100

    remove the react-snap hydration

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom"
 import App from "./components/App"
 import * as serviceWorker from "./serviceWorker"
 
-const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, document.getElementById("root"))
 
-if (rootElement.hasChildNodes()) {
-	ReactDOM.hydrate(<App />, rootElement)
-} else {
-	ReactDOM.render(<App />, rootElement)
-}
+// const rootElement = document.getElementById("root")
+// if (rootElement.hasChildNodes()) {
+// 	ReactDOM.hydrate(<App />, rootElement)
+// } else {
+// 	ReactDOM.render(<App />, rootElement)
+// }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
